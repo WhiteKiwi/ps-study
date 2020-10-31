f4923cb3ac4e9391b65bb739dbc09366d75b53a3
whitekiwi <jh145478@gmail.com>
2020-10-31 16:36:55 +0900

    BOJ #16172

---
+++ 16172.py
@@ -0,0 +1,12 @@
+# 2020-11-01
+# 집합 16172 https://www.acmicpc.net/problem/16172
+import sys
+import re
+
+originStr = sys.stdin.readline()
+targetStr = sys.stdin.readline()
+filteredStr = re.sub(r'[0-9]', '', originStr)
+if (re.search(targetStr, filteredStr)):
+    print(1)
+else:
+    print(0)
