26b9f48aee439a5a70ba788592b9c7cb6f06f24e
whitekiwi <jh145478@gmail.com>
2020-11-22 16:44:54 +0900

    BOJ #2887 #시간초과
    Priority Queue 버전

--- BOJ/2887.py
+++ BOJ/2887.py
@@ -1,6 +1,7 @@
 # 2020-11-21 - map ver - 시간초과
 # 행성 터널 2887 https://www.acmicpc.net/problem/2887
 import sys
+from queue import PriorityQueue
 
 class Node:
     def __init__(self, index, x, y, z):
@@ -9,31 +10,21 @@ class Node:
         self.y = y
         self.z = z
 
+x_list = []
+y_list = []
+z_list = []
+
 def input(node_map, index, x, y, z):
+    global x_list
+    global y_list
+    global z_list
     new_node = Node(index, x, y, z)
+    x_list.append({ "val": x, "index": index })
+    y_list.append({ "val": y, "index": index })
+    z_list.append({ "val": z, "index": index })
     node_map[i] = new_node
 
-def find_min(node_map, val, index_to_exclude, get_dist):
-    minimum_node = None
-    min_dist = sys.maxsize
-    for key in node_map.keys():
-        dist = get_dist(node_map[key], val)
-        if node_map[key].index != index_to_exclude and min_dist > dist:
-            min_dist = dist
-            minimum_node = node_map[key]
-    return minimum_node
-
-def get_dist_x(node, val):
-    return abs(node.x - val)
-
-def get_dist_y(node, val):
-    return abs(node.y - val)
-
-def get_dist_z(node, val):
-    return abs(node.z - val)
-
-def add_edge(edge_map, a, b, arr):
-    dist = min(arr)
+def add_edge(edge_map, a, b, dist, pq = None):
     if a in edge_map:
         if b not in edge_map[a] or dist < edge_map[a][b]:
             edge_map[a][b] = dist
@@ -44,64 +35,58 @@ def add_edge(edge_map, a, b, arr):
             edge_map[b][a] = dist
     else:
         edge_map[b] = { a: dist }
+    if pq != None:
+        pq.put((dist, [a, b]))
+
+def make_pq(edge_map, pq):
+    for key_a in edge_map.keys():
+        for key_b in edge_map[key_a].keys():
+            pq.put((edge_map[key_a][key_b], [key_a, key_b]))
 
 def make_edge_map(node_map, edge_map):
-    for key in node_map.keys():
-        node = node_map[key]
-        closest_node_x = find_min(node_map, node.x, node.index, get_dist_x)
-        closest_node_y = find_min(node_map, node.y, node.index, get_dist_y)
-        closest_node_z = find_min(node_map, node.z, node.index, get_dist_z)
-        dist_x = abs(closest_node_x.x - node.x)
-        dist_y = abs(closest_node_y.y - node.y)
-        dist_z = abs(closest_node_z.z - node.z)
-        if closest_node_x.index == closest_node_y.index:
-            if closest_node_x.index == closest_node_z.index: # x=y=z
-                add_edge(edge_map, closest_node_x.index, node.index, [dist_x, dist_y, dist_z])
-            else: # x=y!=z
-                add_edge(edge_map, closest_node_x.index, node.index, [dist_x, dist_y])
-                add_edge(edge_map, closest_node_z.index, node.index, [dist_z])
-        elif closest_node_x.index == closest_node_z.index: # x=z!=y
-            add_edge(edge_map, closest_node_x.index, node.index, [dist_x, dist_z])
-            add_edge(edge_map, closest_node_y.index, node.index, [dist_y])
-        elif closest_node_y.index == closest_node_z.index: # x!=y=z
-            add_edge(edge_map, closest_node_y.index, node.index, [dist_y, dist_z])
-            add_edge(edge_map, closest_node_x.index, node.index, [dist_x])
-        else: # x!=y!=z
-            add_edge(edge_map, closest_node_x.index, node.index, [dist_x])
-            add_edge(edge_map, closest_node_y.index, node.index, [dist_y])
-            add_edge(edge_map, closest_node_z.index, node.index, [dist_z])
+    global x_list
+    global y_list
+    global z_list
 
-def union(node_map, edge_map, a, b):
+    x_list = sorted(x_list, key = lambda x: x["val"])
+    for i in range(len(x_list) - 1):
+        dist_x = abs(x_list[i]["val"] - x_list[i + 1]["val"])
+        add_edge(edge_map, x_list[i]["index"], x_list[i + 1]["index"], dist_x)
+
+    y_list = sorted(y_list, key = lambda x: x["val"])
+    for i in range(len(y_list) - 1):
+        dist_y = abs(y_list[i]["val"] - y_list[i + 1]["val"])
+        add_edge(edge_map, y_list[i]["index"], y_list[i + 1]["index"], dist_y)
+
+    z_list = sorted(z_list, key = lambda x: x["val"])
+    for i in range(len(z_list) - 1):
+        dist_z = abs(z_list[i]["val"] - z_list[i + 1]["val"])
+        add_edge(edge_map, z_list[i]["index"], z_list[i + 1]["index"], dist_z)
+
+def union(node_map, edge_map, a, b, pq):
+    for key in edge_map[b.index].keys():
+        if key not in node_map:
+            continue
+        node = node_map[key]
+        if node.index != a.index:
+            if node.index not in edge_map[a.index]:
+                # A랑 node랑 연결이 안되어 있었으면 그냥 추가
+                dist_from_b = edge_map[b.index][node.index]
+                add_edge(edge_map, a.index, node.index, dist_from_b, pq)
+            else:
+                dist_from_b = edge_map[b.index][node.index]
+                add_edge(edge_map, a.index, node.index, dist_from_b, pq)
+
     for key in node_map.keys():
         node = node_map[key]
         if node.index != a.index and node.index != b.index and b.index in edge_map and node.index in edge_map[b.index]:
             if node.index not in edge_map[a.index]:
                 # A랑 node랑 연결이 안되어 있었으면 그냥 추가
                 dist_from_b = edge_map[b.index][node.index]
-                add_edge(edge_map, a.index, node.index, [dist_from_b])
+                add_edge(edge_map, a.index, node.index, dist_from_b, pq)
             else:
-                dist_from_a = edge_map[a.index][node.index]
                 dist_from_b = edge_map[b.index][node.index]
-                # a와의 거리를 a, b 중 짧은 것으로 통일하고
-                if dist_from_a > dist_from_b:
-                    add_edge(edge_map, a.index, node.index, [dist_from_b])
-            # b와 연결된 간선 제거
-            del edge_map[b.index][node.index]
-            del edge_map[node.index][b.index]
-    del edge_map[b.index][a.index]
-    del edge_map[a.index][b.index]
-
-def get_min_edge(edge_map):
-    min_a = None
-    min_b = None
-    min_dist = sys.maxsize
-    for key_a in edge_map.keys():
-        for key_b in edge_map[key_a]:
-            if edge_map[key_a][key_b] < min_dist:
-                min_dist = edge_map[key_a][key_b]
-                min_a = key_a
-                min_b = key_b
-    return [min_a, min_b, min_dist]
+                add_edge(edge_map, a.index, node.index, dist_from_b, pq)
 
 N = int(sys.stdin.readline())
 node_map = {}
@@ -111,13 +96,17 @@ for i in range(N):
     input(node_map, i, x, y, z)
 
 make_edge_map(node_map, edge_map)
+pq = PriorityQueue()
+make_pq(edge_map, pq)
 
 cost = 0
-for i in range(N-1):
-    [a, b, min_dist] = get_min_edge(edge_map)
-    cost += min_dist
-    union(node_map, edge_map, node_map[a], node_map[b])
-    del node_map[b]
+while len(node_map) > 1:
+    data = pq.get()
+    [a, b] = data[1]
+    if a in node_map and b in node_map:
+        cost += data[0]
+        union(node_map, edge_map, node_map[a], node_map[b], pq)
+        del node_map[b]
 
 print(cost)
 
