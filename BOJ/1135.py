ee43898cb9537b9ec9e8bbb9f96a98291c35fecd
장지훈 <jh145478@gmail.com>
2021-04-30 17:47:24 +0900

    style(BOJ 1135): Lint codes

--- BOJ/1135.py
+++ BOJ/1135.py
@@ -6,7 +6,6 @@ class Node:
     def __init__(self, depth):
         self.depth = depth
         self.children = []
-        self.children_length = 0
         self.time = 0
 
 def travel(node):
@@ -15,14 +14,14 @@ def travel(node):
         child.time = travel(child)
     node.children.sort(key = lambda x: x.time, reverse=True)
     
-    max_time_of_node = 0
+    max_time = 0
     for i, child in enumerate(node.children):
-        # 1(현재 노드에 요청 보내는 시간) + child.time + index의 최댓값이 이 노드의 최소 시간
-        # 요청을 돌리면 그 노드도 요청을 시작하므로 (자식이 필요한 시간 + 정렬 후 순서)의 최댓값이 최소 시간이라고 할 수 있음
-        if i + child.time + 1 > max_time_of_node:
-            max_time_of_node = i + child.time + 1
+        # i + 1(현재 노드에 요청 보내기까지 걸린 시간) + child.time
+        # 요청을 받은 순간부터 요청을 시작하므로 (노드가 수행되는데 필요한 시간 + 요청 순서)의 최댓값이 최소로 필요한 시간이라고 할 수 있음
+        if i + 1 + child.time > max_time:
+            max_time = i + 1 + child.time
 
-    return max_time_of_node
+    return max_time
 
 node_list = []
 # N = int(sys.stdin.readline())
@@ -37,13 +36,11 @@ for parent_index in parent_indices:
         continue
 
     parent_node = node_list[parent_index]
-    # 부모 children_length ++
-    parent_node.children_length += 1
 
     # 부모 depth + 1로 child 생성
     node = Node(parent_node.depth + 1)
-    parent_node.children.append(node)
     node_list.append(node)
+    parent_node.children.append(node)
 
 max_time = travel(node_list[0])
 
